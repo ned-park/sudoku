b0e2de9e2163e6bf21bff8698afbe11e4b8e34fd
ned-park <87483870+ned-park@users.noreply.github.com>
2024-02-06 21:56:05 -0500

    refactor: simplify board display

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,38 +1,23 @@
 export default function Board({ board, setBoard, isMutable, isValid }) {
-  let tilesToDisplay = new Array(9).fill(0).map(() => new Array(9).fill(""));
-  for (let i = 0; i < board.length; i += 9) {
-    for (let j = 0; j < 9; j++) {
-      tilesToDisplay[i / 9][j] = [
-        board[i + j] > 0 ? board[i + j] : "",
-        isMutable[i + j],
-        isValid[i + j],
-      ];
-    }
-  }
-
   function updateBoard(e) {
     let idx = e.target.id.split("-")[1];
-    setBoard((oldBoard) =>
-      oldBoard.map((val, i) => (i == idx ? Number(e.target.value) : val))
-    );
+    let newValue = e.target.value >= 1 && e.target.value <= 9 ? Number(e.target.value) : 0;
+    setBoard((oldBoard) => oldBoard.map((value, i) => (i == idx ? newValue : value)));
   }
 
   return (
     <div className="board">
-      {tilesToDisplay.map((row, m) => (
-        <div className="row" key={`row-${m}`}>
-          {row.map((col, n) => (
-            <input
-              id={`square-${9 * m + n}`}
-              key={`square-${m}-${n}`}
-              className={`square ${isValid[9 * m + n] ? "" : "wrong"}`}
-              disabled={!col[1]}
-              // placeholder={}
-              value={col[0]}
-              onChange={(e) => updateBoard(e)}
-            />
-          ))}
-        </div>
+      {board.map((sq, m) => (
+        <input
+          id={`square-${m}`}
+          key={`square-${m}`}
+          className={`square ${isValid[m] ? "" : "wrong"} ${
+            Math.floor((m - (m % 9) + 9) % 27) != 0 ? "" : "boxbottom"
+          }`}
+          disabled={!isMutable[m]}
+          value={sq != 0 ? sq : ""}
+          onChange={(e) => updateBoard(e)}
+        />
       ))}
     </div>
   );
