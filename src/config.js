43b3fc250ca8b2d22678065b54d3b387beddfbd3
ned-park <87483870+ned-park@users.noreply.github.com>
2024-05-12 09:54:05 -0400

    build: update caprover deploy

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
   update to https://stackoverflow.com/questions/71543415/how-to-change-the-url-prefix-for-fetch-calls-depending-on-dev-vs-prod-environmen
 */
 
-export const __SITE_PREFIX__ = "https://sudoku-backend-eta.vercel.app"; // import.meta.env.VITE_SITE_PREFIX ?? "http://localhost:3001";
+export const __SITE_PREFIX__ = import.meta.env.VITE_SITE_PREFIX ?? "http://localhost:3001";
