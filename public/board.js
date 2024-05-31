4fafa2a796830827331fb54be075a1ccb4b5a983
ned-park <87483870+ned-park@users.noreply.github.com>
2024-05-31 12:30:53 -0400

    refactor: optimize shuffling algorithm

--- public/board.js
+++ public/board.js
@@ -68,8 +68,9 @@ function hasMultipleSolutions(board) {
 }
 
 function randomize(array) {
-  for (let i = 0; i < array.length * 2; i++) {
-    array.sort(() => Math.random() - 0.5);
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
